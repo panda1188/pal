4a3dd2bc6e9aea944d9d5eb46d3863b7ce70f9bb
panda1188 <31615902+panda1188@users.noreply.github.com>
2017-09-28 09:06:25 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 # guess what this prog does??
 import random
+#system choses the random numbers 
 r=random.randint(1,6) #give the dise value
+#random numbers from 1 to 6
 print (r)
+#computers prints the random number
 if r==6 :
+ #random number is 6
  print ("congratulation you got 6 ")
+ #system gives the result
