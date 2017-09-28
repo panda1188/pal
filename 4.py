3cfa1f7d6ff5e1fdd08dc39f5bcfd2c39e909bee
panda1188 <31615902+panda1188@users.noreply.github.com>
2017-09-28 09:25:38 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 num = int(input("enter the number:  "))
+#we need to import a number 
 for i in range(1,4):
-	for j in range(1,11):
+	# i is range from 1 to 4
+	for j in range(1,11): 
+		#this is the range from 1 to 11
 		print(i,"x",j,"=",i*j)
+		#the result is seen after running the program
